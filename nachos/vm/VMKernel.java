19a94afee0355dff22d46e200f245cd6bfc06a7d
Zheng <83680282+RhythmGrind@users.noreply.github.com>
2023-11-27 11:18:16 -0800

    Update VMKernel.java

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -4,24 +4,63 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+import java.util.LinkedList;
 
 /**
  * A kernel that can support multiple demand-paging user processes.
  */
 public class VMKernel extends UserKernel {
+	private static LinkedList<Integer> freePages;
+	private static OpenFile swapFile;
+    private static LinkedList<Integer> freeSwapPages;
+    private static Lock vmmutex;
+
 	/**
 	 * Allocate a new VM kernel.
 	 */
 	public VMKernel() {
 		super();
+		System.out.println("VMKernel created.");
+
 	}
 
 	/**
 	 * Initialize this kernel.
 	 */
-	public void initialize(String[] args) {
-		super.initialize(args);
-	}
+    public void initialize(String[] args) {
+        super.initialize(args);
+        freePages = new LinkedList<>();
+
+        int numPhysPages = Machine.processor().getNumPhysPages();
+        System.out.println("Initializing free pages list with " + numPhysPages + " pages.");
+        for (int i = 0; i < numPhysPages; i++) {
+            freePages.add(i);
+        }
+
+        // Initialize swap file management
+        swapFile = ThreadedKernel.fileSystem.open("swapFile", true);
+        freeSwapPages = new LinkedList<Integer>();
+        vmmutex = new Lock();
+    }
+
+    public static synchronized int allocatePage() {
+        if (!freePages.isEmpty()) {
+            int allocatedPage = freePages.removeFirst();
+            System.out.println("Allocating page: " + allocatedPage);
+            return allocatedPage;
+        }
+        System.out.println("VMKernel: No free pages available.");
+        return -1;
+    }
+
+    public static synchronized void freePage(int pageNumber) {
+        if (pageNumber < 0 || pageNumber >= Machine.processor().getNumPhysPages()) {
+            System.out.println("VMKernel: Attempted to free invalid page number " + pageNumber);
+            return;
+        }
+        freePages.add(pageNumber);
+        System.out.println("VMKernel: Freed page " + pageNumber);
+    }
 
 	/**
 	 * Test this kernel.
@@ -41,7 +80,9 @@ public class VMKernel extends UserKernel {
 	 * Terminate this kernel. Never returns.
 	 */
 	public void terminate() {
-		super.terminate();
+		swapFile.close();
+        ThreadedKernel.fileSystem.remove("swapFile");
+        super.terminate();
 	}
 
 	// dummy variables to make javac smarter
